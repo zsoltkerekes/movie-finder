128395152d43c529608f38059d28527b37661b3b
Zsolt Kerekes <zsolt_kerekes@epam.com>
2018-10-05 16:21:25 +0200

    upgraded @angular version, and improve mobil design

--- src/app/services/scroller.service.ts
+++ src/app/services/scroller.service.ts
@@ -6,13 +6,14 @@ export class ScrollerService {
   constructor() {
   }
 
-  toTop(time = 1000) {
-    const scrollVelocity = window.scrollY * (1 / time) * 12;
+  toTop(time = 1000, speedIndex = 12) {
+    const height = () => window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    const scrollVelocity = height() * (1 / time) * speedIndex;
     const interval = setInterval(
       () => {
         document.body.scrollTop -= scrollVelocity;
         document.documentElement.scrollTop -= scrollVelocity;
-        if (window.scrollY <= 0) {
+        if (!window.pageYOffset) {
           clearInterval(interval);
         }
       });
