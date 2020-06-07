d586c8111a304d27c1c9fee453ec4f2f84425bea
Kerekes Zsolt <kerekes.zsolt.job@gmail.com>
2020-06-07 13:31:42 +0200

    added basic e2e tests for all the already refactored pages

---
+++ e2e/person-details.e2e-spec.ts
@@ -0,0 +1,95 @@
+import { PersonDetailsPage } from './person-details.po';
+
+describe('movie-finder person details page', () => {
+  let page: PersonDetailsPage;
+  page = new PersonDetailsPage();
+
+  beforeAll(async () => {
+    await page.navigateTo();
+  });
+
+  it('should display "Movie Finder" title in the header', () => {
+    expect(page.titleText()).toEqual('Movie Finder');
+  });
+
+  it('should display person poster', () => {
+    expect(page.personPoster().count()).toEqual(1);
+  });
+
+  it('should display person details', () => {
+    expect(page.personDetails().count()).toEqual(1);
+  });
+
+  it('should display person name', () => {
+    expect(page.personName().count()).toEqual(1);
+  });
+
+  it('should display person overview', () => {
+    expect(page.personOverview().count()).toEqual(1);
+  });
+
+  it('should display search queries', () => {
+    expect(page.personQueries().count()).toEqual(6);
+  });
+
+  it('should display person images', () => {
+    expect(page.personImages().count()).toEqual(1);
+  });
+
+  it('should display person tagged images', () => {
+    expect(page.personTaggedImages().count()).toEqual(1);
+  });
+
+  it('should display person movie credits', () => {
+    expect(page.personMovieCredits().count()).toEqual(1);
+  });
+
+  it('should display person movie credits cast', () => {
+    expect(page.personMovieCreditsCast().count()).toBeGreaterThanOrEqual(1);
+  });
+
+  it('should have a working person movie credits cast filtering', () => {
+    page.setInputToPersonMovieSearchCastFieldText('syl');
+    expect(page.personMovieCreditsCast().count()).toEqual(2);
+  });
+
+  it('should display person movie credits crew', () => {
+    expect(page.personMovieCreditsCrew().count()).toBeGreaterThanOrEqual(1);
+  });
+
+  it('should have a working person movie credits crew filtering', () => {
+    page.setInputToPersonMovieSearchCrewFieldText('story');
+    expect(page.personMovieCreditsCast().count()).toEqual(2);
+  });
+
+  it('should display person tv credits', () => {
+    expect(page.personTvCredits().count()).toEqual(1);
+  });
+
+  it('should display person tv credits cast', () => {
+    expect(page.personTvCreditsCast().count()).toBeGreaterThanOrEqual(1);
+  });
+
+  it('should have a working person tv credits cast filtering', () => {
+    page.setInputToPersonTvSearchCastFieldText('pet');
+    expect(page.personTvCreditsCast().count()).toEqual(2);
+  });
+
+  it('should display person tv crew', () => {
+    expect(page.personTvCreditsCrew().count()).toBeGreaterThanOrEqual(1);
+  });
+
+  it('should have a working person tv credits crew filtering', () => {
+    page.setInputToPersonTvSearchCrewFieldText('contender');
+    expect(page.personTvCreditsCast().count()).toEqual(2);
+  });
+
+  it('should display "Movie Finder" title in the footer', () => {
+    expect(page.footerText()).toContain('Movie Finder');
+  });
+
+  it('should display button to top', async () => {
+    const amount = await page.buttonToTop().count();
+    expect(amount).toEqual(1);
+  });
+});
---
+++ e2e/person-details.po.ts
@@ -0,0 +1,91 @@
+import { browser, by, element } from 'protractor';
+
+export class PersonDetailsPage {
+  navigateTo() {
+    return browser.get('#/person-details/16483');
+  }
+
+  titleText() {
+    return element(by.css('mf-root h1')).getText();
+  }
+
+  personPoster() {
+    return element.all(by.css('mat-card.poster img'));
+  }
+
+  personDetails() {
+    return element.all(by.css('mat-card.details'));
+  }
+
+  personName() {
+    return element.all(by.css('mat-card.details h2.title'));
+  }
+
+  personOverview() {
+    return element.all(by.css('mat-card.details p.overview'));
+  }
+
+  personQueries() {
+    return element.all(by.css('mat-card.details .query'));
+  }
+
+  personImages() {
+    return element.all(by.css('mf-person-images img'));
+  }
+
+  personTaggedImages() {
+    return element.all(by.css('mf-person-tagged-images img'));
+  }
+
+  personMovieCredits() {
+    return element.all(by.css('mf-person-movie-credits'));
+  }
+
+  personMovieCreditsCast() {
+    return element.all(by.css('mf-person-movie-credits .cast a'));
+  }
+
+  setInputToPersonMovieSearchCrewFieldText(searchPhrase: string) {
+    element(by.css('mf-person-movie-credits #searchCrew')).sendKeys(
+      searchPhrase
+    );
+  }
+
+  personMovieCreditsCrew() {
+    return element.all(by.css('mf-person-movie-credits .crew a'));
+  }
+
+  setInputToPersonMovieSearchCastFieldText(searchPhrase: string) {
+    element(by.css('mf-person-movie-credits #searchCast')).sendKeys(
+      searchPhrase
+    );
+  }
+
+  personTvCredits() {
+    return element.all(by.css('mf-person-tv-credits'));
+  }
+
+  personTvCreditsCast() {
+    return element.all(by.css('mf-person-tv-credits .cast a'));
+  }
+
+  setInputToPersonTvSearchCrewFieldText(searchPhrase: string) {
+    element(by.css('mf-person-tv-credits #searchCrew')).sendKeys(searchPhrase);
+  }
+
+  personTvCreditsCrew() {
+    return element.all(by.css('mf-person-tv-credits .crew a'));
+  }
+
+  setInputToPersonTvSearchCastFieldText(searchPhrase: string) {
+    element(by.css('mf-person-tv-credits #searchCast')).sendKeys(searchPhrase);
+  }
+
+  buttonToTop() {
+    return element.all(by.css('button.toTop'));
+  }
+
+  footerText() {
+    return element(by.css('mf-root mf-footer p')).getText();
+  }
+}
