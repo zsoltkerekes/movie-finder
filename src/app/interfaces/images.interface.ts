0ae0f8ffe02f78102d741af09d89e502094a6c2c
Kerekes Zsolt <kerekes.zsolt.job@gmail.com>
2020-10-06 14:42:22 +0200

    design change

--- src/app/interfaces/images.interface.ts
+++ src/app/interfaces/images.interface.ts
@@ -1,7 +1,17 @@
 export interface Images {
   file_path: string;
   vote_average: number;
-  media: unknown;
+  media: {
+    name?: string;
+    title?: string;
+    id?: number;
+    original_name?: string;
+    original_title?: string;
+    release_date?: string;
+    first_air_date?: string;
+    overview?: string;
+    vote_average?: number;
+  };
   media_type: string;
 }
 
